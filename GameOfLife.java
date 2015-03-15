294af933f707e43b0449bb32b1b3271cc403f738
Junaid Haroon Siddiqui <junaid.siddiqui@lums.edu.pk>
2015-03-15 12:11:47 +0500

    working single threaded

--- GameOfLife.java
+++ GameOfLife.java
@@ -9,12 +9,14 @@ public class GameOfLife {
 	String seedFile;
 	String path = "C:\\Users\\babar naveed\\Desktop\\ap_git\\15100142\\midTerm\\";
 	Cell[][] gridOfLife;
+	Cell[][] updateLife;
 
 	public GameOfLife(Integer sizeOfGrid, Integer iteration, String seedFile) {
 		this.sizeOfGrid = sizeOfGrid;
 		this.iteration = iteration;
 		this.seedFile = path + seedFile;
 		gridOfLife = new Cell[sizeOfGrid][sizeOfGrid];
+		updateLife = new Cell[sizeOfGrid][sizeOfGrid];
 	}
 
 	public void seed() {
@@ -35,6 +37,7 @@ public class GameOfLife {
  						cell = new Cell(true);
  					}
  					gridOfLife[i][j] = cell;
+
  					j++;
  				}}
  				i++;
@@ -50,7 +53,13 @@ public class GameOfLife {
  			// 	}
  			// 	System.out.println();
  			// }
-
+ 			for (int a=0; a<sizeOfGrid; a++) {
+ 				for (int b=0; b<sizeOfGrid; b++) {
+ 					Cell cell = new Cell(true);
+ 					updateLife[a][b] = cell;
+ 				}
+ 			}
+ 		copyGridOfLife();
  		}catch(Exception e) {
  			e.printStackTrace();
  		} finally{
@@ -71,6 +80,79 @@ public class GameOfLife {
 			}
 			System.out.println();
 		}
+		System.out.println();
+		System.out.println();
+	}
+
+	public void copyUpdateLife() {
+		for (int i=0; i<sizeOfGrid; i++) {
+			for (int j=0; j<sizeOfGrid; j++) {
+				gridOfLife[i][j].update(updateLife[i][j].checkStatus());
+			}
+		}
+	}
+
+	public void copyGridOfLife() {
+		for (int i=0; i<sizeOfGrid; i++) {
+			for (int j=0; j<sizeOfGrid; j++) {
+				updateLife[i][j].update(gridOfLife[i][j].checkStatus());
+				// updateLife[i][j] = gridOfLife[i][j];
+			}
+		}
+	}
+
+	public void runIterations() throws Exception {
+		// System.out.println("in runs");
+		for (int i=0; i<this.iteration; i++) {
+			runIteration();
+		}
+	}
+
+	public int calculateNeighbours(int row, int col) throws Exception {
+		// System.out.println("in calculate");
+		if(row >= sizeOfGrid || col >= sizeOfGrid || row < 0 || col < 0) {
+			throw new Exception("out of bounds");
+		}
+		// System.out.println("past exception " + row + " " + col);
+		int count = 0;
+		for (int i = row - 1; i <= row + 1; i++) {
+			for (int j = col - 1; j <= col + 1; j++) {
+				// System.out.println("in loop " + i + " " + j);
+				if(i >= sizeOfGrid || j >= sizeOfGrid || i < 0 || j < 0) {} else {
+					if (gridOfLife[i][j].checkStatus()) {
+						if (i == row && j == col) {} else {
+							// System.out.println(" " + i + " " + j + "  increment");
+							count++;
+						}
+					}
+				}
+			}
+		}
+		// System.out.println(count);
+		return count;
+	}
+
+	public void runIteration() throws Exception {
+		// System.out.println("in run");
+		copyGridOfLife();
+		for (int i=0; i<sizeOfGrid; i++) {
+			for (int j=0; j<sizeOfGrid; j++) {
+				int neighbours = calculateNeighbours(i,j);
+				if (neighbours < 2) {
+					updateLife[i][j].update(false);
+				} else if (neighbours > 3) {
+					updateLife[i][j].update(false);
+				} else if (neighbours == 3) {
+					updateLife[i][j].update(true);
+				} else if (neighbours >=2 && neighbours <= 3 && gridOfLife[i][j].checkStatus()) {
+					updateLife[i][j].update(true);
+				} else {
+					updateLife[i][j].update(false);
+				}
+			}
+		}
+		copyUpdateLife();
+		print();
 	}
 
 	public static void main(String[] args) throws Exception {
@@ -90,5 +172,8 @@ public class GameOfLife {
 		GameOfLife gol = new GameOfLife(n,g,file);
 		gol.seed();
 		gol.print();
+		gol.runIterations();
+		
+		
 	}
 }
